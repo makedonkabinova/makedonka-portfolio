67b38dca90d3f05149966575c05a78c1e150939e
makedonkabinova <makedonka.binova@student.um.si>
2023-10-12 17:22:40 +0200

    Better responsiveness on phone

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -2,12 +2,11 @@ import './index.scss'
 
 const ProjectCard = ({ title, imageUrl, githubLink, description }) => {
   return (
-    <div className="project-card">
+    <a href={githubLink} className="project-card">
       <h3>{title}</h3>
       <img src={imageUrl} alt={title} />
       <p>{description}</p>
-      <a href={githubLink}>GitHub Link</a>
-    </div>
+    </a>
   );
 }
 
